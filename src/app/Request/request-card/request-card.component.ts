a204f87b6b7981d9820ff45e4fe1a02b741869e4
Tarik Hasan <tarik.hasan@mni.thm.de>
2022-06-28 12:29:09 +0200

    Request Karten erstellt

--- src/app/Request/request-card/request-card.component.ts
+++ src/app/Request/request-card/request-card.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {AuthService} from '../../../services/auth.service';
+import {Router} from '@angular/router';
+import {NavController} from '@ionic/angular';
+import {ToastService} from '../../../services/toast.service';
+import {UserService} from '../../../services/user.service';
+import {RequestService} from '../../../services/request.service';
+import {User} from '../../../model/user';
+import {Request} from '../../../model/request';
+
 
 @Component({
   selector: 'app-request-card',
@@ -6,9 +15,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./request-card.component.scss'],
 })
 export class RequestCardComponent implements OnInit {
+  @Input() request: Request;
+  user: User;
+  constructor(private requestService: RequestService, private authService: AuthService, private router: Router,
+              private nacCtrl: NavController, private toastService: ToastService, public userService: UserService) {
+  }
 
-  constructor() { }
-
-  ngOnInit() {}
-
+  ngOnInit() {
+    this.userService.getUserById(this.request.userId).then((user) => {
+      this.user = user;
+    });
+  }
+  openRequestDetail(requestId: string) {
+    this.router.navigate(['request-detail', {requestId: JSON.stringify(requestId)}]);
+  }
+  openOtherUser(userId: string){
+    this.userService.setOtherUser(userId);
+    this.router.navigate(['otherUser', {userId: JSON.stringify(userId)}]);
+  }
 }
