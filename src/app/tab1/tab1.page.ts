9325e5e1897f30a527af1480b446be1eca6afd18
davidb <david.bausch@mni.thm.de>
2022-08-12 12:12:19 +0200

    Bugfix: Check if user has a vehicle, before navigating to create offer page

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -5,6 +5,9 @@ import {NavController} from '@ionic/angular';
 import {ToastService} from '../../services/toast.service';
 import {UserService} from '../../services/user.service';
 import {OfferService} from '../../services/offer.service';
+import {VehicleService} from "../../services/vehicle.service";
+import {Vehicle} from "../../model/vehicle";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-tab1',
@@ -12,21 +15,37 @@ import {OfferService} from '../../services/offer.service';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page implements OnInit{
+  vehicles: Vehicle[]
+  vehicleObserve: Observable<Vehicle[]>
 
   constructor(public authService: AuthService, private router: Router, private nacCtrl: NavController,
-              public offerService: OfferService, private toastService: ToastService, public userService: UserService) {}
+              public offerService: OfferService, private toastService: ToastService, public userService: UserService,
+              public vehicleService: VehicleService) {}
 
-  async ngOnInit(){}
+  async ngOnInit(){
+  }
 
   /**
    * Navigating to login page if the user is not logged in or to the creating page
    */
   createOffer() {
-    if(this.authService.user === null){
-      this.router.navigate(['login']);
-      this.toastService.presentToast('Um ein Angebot zu erstellen, musst du dich anmelden!', 'danger');
-    }else{
-      this.router.navigate(['create-offer']);
-    }
+    //get vehicles of logged in user
+    this.vehicleService.getVehicles().then(res => {
+      this.vehicleObserve = res;
+      this.vehicleObserve.subscribe(vehicles => {
+        this.vehicles = vehicles;
+      });
+      if(this.authService.user === null){
+        this.router.navigate(['login']).then();
+        this.toastService.presentToast('Um ein Angebot zu erstellen, musst du dich anmelden!', 'danger').then();
+      }else if(this.vehicles.length === 0){
+        this.router.navigate(['profile']).then();
+        this.toastService.presentToast('Um ein Angebot zu erstellen, füge deinem Profil bitte ein' +
+          ' Fahrzeug hinzu!', 'danger').then();
+      }
+      else{
+        this.router.navigate(['create-offer']).then();
+      }
+    });
   }
 }
